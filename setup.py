c20f7e844053f86e5d91dc3f6430a9902a71b6e3
daquang <daniel.quang@gmail.com>
2013-08-13 23:11:54 -0700

    Cython setup

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+ext_modules=[Extension("meme",["memewrapper.pyx","likelihood.c","llr.c"])]
+
+setup(name = "MEME app",cmdclass = {"build_ext": build_ext},ext_modules = ext_modules)
